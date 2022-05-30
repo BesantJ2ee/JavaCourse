83cbc0858c37bf912ad2c38bb65b0ec4ebc1e74f
Gamma <simonjustin321@gmail.com>
2022-05-30 21:37:36 +0530

    30_05_2022

--- EserciseFile/src/com/besant/Chapter2/ExerciseFile_InputFudamentalsAndDatatype_Chapter3.java
+++ EserciseFile/src/com/besant/Chapter2/ExerciseFile_InputFudamentalsAndDatatype_Chapter3.java
@@ -6,21 +6,22 @@ public class ExerciseFile_InputFudamentalsAndDatatype_Chapter3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner myObj = new Scanner(System.in);
-
-	    System.out.println("Enter name, age and salary:");
-
-	    // String input
-	    String name = myObj.nextLine();
-
-	    // Numerical input
-	    int age = myObj.nextInt();
-	    double salary = myObj.nextDouble();
-
+	//Scanner
+		
+		Scanner  scannerObject = new Scanner(System.in);
+		System.out.println("Enter name, age and salary:");
+		
+		
+		String name = scannerObject.nextLine();
+		int age = scannerObject.nextInt();
+		 double salary = scannerObject.nextDouble();
+		
 	    // Output input by user
 	    System.out.println("Name: " + name);
 	    System.out.println("Age: " + age);
 	    System.out.println("Salary: " + salary);
+	    
+	 // 
 	}
 
 }
---
+++ EserciseFile/src/com/besant/Chapter2/Rahul/Arrray_Discussion.java
@@ -0,0 +1,76 @@
+package com.besant.Chapter2.Rahul;
+
+public class Arrray_Discussion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		int[] a= {1,2,5,8,1};
+		
+		int[] b= {3,2,6,8,7};
+		
+		//String array
+		
+		
+		String[]  k= {"Abv","ssf","fsnsdnkcs"};	
+		
+		String abc1=null;
+		String employee="besant";
+		
+		String[]  k1= {"wipro","besant","cts"};	
+		
+		/*
+		 * for(int i=0;i<k1.length;i++) { if(employee.contains(k1[i])) {
+		 * System.out.println("The company name is ="+k1[i]+"Contains Method"); } }
+		 */
+		
+		/*
+		 * for(int i=0;i<k1.length;i++) { if(employee.equalsIgnoreCase(k1[i])) {
+		 * System.out.println("The company name is ="+k1[i]+"Equalsignorecase method");
+		 * }else { System.out.println("The company name is not availble in the list"); }
+		 * }
+		 */
+		
+		
+		char[] charter = {'B','O','X'};
+		
+		String xv = null;
+		String y = null;
+		String z = null;
+		char abx = 'c';
+		
+		Character xchar = 'h';
+		
+		Character ychar = 'b';
+		Character zchar = 'g';
+		
+		
+		for(int i=0;i<charter.length;i++)
+		{
+		if(charter[i]=='B')
+		{
+		xchar=  charter[i];	
+		}
+		if(charter[i]=='O')
+		{
+			ychar=charter[i];	
+		}
+		if(charter[i]=='X')
+		{
+			zchar=charter[i];	
+		}		
+		}
+		System.out.println("the out put is= "+xchar.toString()+ychar.toString()+zchar.toString());
+	}
+
+	//the out put is =BOX
+	
+	//Option a  wiprobesantcts
+	//Option b  wipro besant cts
+	//Option c  wipro 
+	//Option d  null
+	//Option e  cts
+	
+}
